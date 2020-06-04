a388449c127894cf1558e289c14e03fcd95ebd16
hall500 <hallhomoms22@gmail.com>
2020-06-04 02:46:39 +0100

    Email added for abdullah momoh

--- scripts/abdullah-momoh.php
+++ scripts/abdullah-momoh.php
@@ -4,10 +4,10 @@
     private $lastname = 'Momoh';
     private $id = 'HNG-00030';
     private $language = 'PHP';
-    private $email = 'momohabdullah20@gmail.com';
+    private $mail = 'momohabdullah20@gmail.com';
 
     public function output(){
-      return "Hello World, this is {$this->firstname} {$this->lastname} with HNGi7 ID {$this->id} and email {$this->email} using {$this->language} for stage 2 task";
+      return "Hello World, this is {$this->firstname} {$this->lastname} with HNGi7 ID {$this->id} and email {$this->mail} using {$this->language} for stage 2 task";
     }
   }
 
